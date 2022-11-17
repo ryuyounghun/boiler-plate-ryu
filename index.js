42fe7e76f9811b3437558a916540a3a044723c31
ryu <a8300a@gmail.com>
2022-11-17 17:17:55 +0900

    처음올림

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://bogun:yh0707@cluster0.p63lohe.mongodb.net/?retryWrites=true&w=majority',{
+    useNewUrlParser: true, useUnifiedTopology: true
+}).then(() => console.log("MongoDB Connected..."))
+  .catch(err => console.log(err))
+
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
